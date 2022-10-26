cc435742883476cba42ba69876b1bf798e75ec44
sprokaev <prokaevsergey@mail.ru>
2022-10-26 19:38:18 +0500

    added project

---
+++ src/main/java/ru/hogwarts/school/SkyProHw32CreatingApiApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProHw32CreatingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProHw32CreatingApiApplication.class, args);
+    }
+
+}
