974529f186eb299a74d6dbd762daf3524a1ff479
sprokaev <prokaevsergey@mail.ru>
2022-11-20 18:51:25 +0500

    added CONSTRAINT unique_name_color in file 'scripts422.sql'

--- scripts421.sql
+++ scripts421.sql
@@ -3,3 +3,6 @@ ALTER TABLE student
     ALTER COLUMN name SET NOT NULL,
     ADD CONSTRAINT unique_name UNIQUE (name),
     ALTER COLUMN age SET DEFAULT 20;
+
+ALTER TABLE faculty
+    ADD CONSTRAINT unique_name_color UNIQUE (name, color);
